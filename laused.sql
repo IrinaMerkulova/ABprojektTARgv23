026af015d2ac183e66c185b962a9c26adc4434d6
alexkaasik <aleks.kaasik@outlook.com>
2024-04-10 10:48:25 +0300

    lisa teksti: ostime max vanus

--- laused.sql
+++ laused.sql
@@ -152,7 +152,7 @@ select sum(cast(Age as int)) from Person
 --- ostime min vanus
 select min(cast(Age as int)) from Person
 
---?
+--- ostime max vanus
 select max(cast(Age as int)) from Person
 
 select City, sum(cast(Age as int)) as TotalAge from Person group by City
