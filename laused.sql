a382f029b75891ef6aa2be2cca38dfc71a9d1fa0
Martin Rosman <soopka@gmail.com>
2024-04-10 10:55:16 +0300

    show all values from Employees

--- laused.sql
+++ laused.sql
@@ -283,6 +283,7 @@ where Id = 10
 select Id, coalesce(FirstName, MiddleName, LastName) as Name
 from Employees
 
+--- show all values from Employees
 select * from Employees
 
 select * from Department
