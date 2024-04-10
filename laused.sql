7dc5ee32ffe8c383ad570e3dab7959a3a2bd7007
alexkaasik <aleks.kaasik@outlook.com>
2024-04-10 10:46:33 +0300

    lisa teksti: sum kõik Age

--- laused.sql
+++ laused.sql
@@ -146,7 +146,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+--- sum kõik Age
 select sum(cast(Age as int)) from Person
 
 --?
