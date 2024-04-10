621d040be6bef29c175e5afd54f3f5578080721c
alexkaasik <aleks.kaasik@outlook.com>
2024-04-10 10:11:05 +0300

    lisa teksti: andmete kustutamine tabelisse ja vaatame andmete

--- laused.sql
+++ laused.sql
@@ -86,7 +86,7 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 160)
 
---?
+--- andmete kustutamine tabelisse ja vaatame andmete
 select * from Person
 go
 delete from Person where Id = 8
