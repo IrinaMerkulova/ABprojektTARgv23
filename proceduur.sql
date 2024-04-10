df5dfc7fe82f0da52e567809162b62f7248e9120
Mihhail Bajandin <misabajandinov@gmail.com>
2024-04-10 11:04:29 +0300

    sql baasi lause ja proceduur tabeli uuendamine ja andmete sisestamine

--- proceduur.sql
+++ proceduur.sql
@@ -10,7 +10,7 @@ spGetEmployees
 exec spGetEmployees
 execute spGetEmployees
 
---- 
+--- Loo protseduur töötajate filtreerimiseks
 create proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
 @DepartmentId int
@@ -20,13 +20,13 @@ as begin
 end
 
 --- kõik esimeses osakonnas meessoost töötavad isikud
-spGetEmployeesByGenderAndDepartment 'Male', 1
+exec spGetEmployeesByGenderAndDepartment 'Male', 1
 
-spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
+ exec spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
---?
+-- Loo protseduur soolise töötajate arvu saamiseks.
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
 @EmployeeCount int output
@@ -34,7 +34,7 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- naeb kui palju naised on employee
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
@@ -48,7 +48,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+--- procedure loomine 
 create proc spTotalCount2
 @TotalCount int output
 as begin
@@ -59,7 +59,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- leiab töötaja eesnime ID järgi
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
@@ -67,19 +67,19 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+--identifitseeri nimi id järgi
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+-- procedure loomine
 create proc spGetNameById2
 @Id int
 as begin
 	return (select FirstName from Employees where Id = @Id)
 end
 
--- ?
+--tagastab töötaja eesnime ID alusel
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
 print 'Name of the employee = ' + @EmployeeName
