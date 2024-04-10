7b6b314a8d989a72f286b74bbcd66743081e0de4
Opilane TTHK <opilane@tthk.ee>
2024-04-10 10:48:43 +0300

    otsige tabelisse Person linna esimese tähega 'n' ja email

--- laused.sql
+++ laused.sql
@@ -110,7 +110,7 @@ Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
 
 
---- ?
+--- otsige tabelisse Person linna esimese tähega 'n' ja email 
 select * from Person where City like 'n%'
 select * from Person where Email like '%@%'
 
