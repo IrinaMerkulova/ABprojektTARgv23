c3c94d59a6e21d50c6a10c6916eb4bad9bab81d4
Opilane TTHK <opilane@tthk.ee>
2024-04-10 10:45:36 +0300

    kirjuta otsing tabelisse Person kust 'Age' =100 või 50 või 20

--- laused.sql
+++ laused.sql
@@ -104,7 +104,7 @@ select * from Person where City = 'Gotham'
 select * from Person where City <> 'Gotham'
 select * from Person where City != 'Gotham'
 
--- ?
+-- kirjuta otsing tabelisse Person kust 'Age' =100 või 50 või 20 
 select *from Person where Age = 100 or 
 Age = 50 or Age = 20
 select * from Person where Age in (100, 50, 20)
