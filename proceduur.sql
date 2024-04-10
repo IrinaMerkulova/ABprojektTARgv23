279f118dcd892ff4903fa140087ff42b835b1cda
Jegor Skljar <jegorka2002@gmail.com>
2024-04-10 10:57:45 +0300

    fail proceduur on uuendatud ja lisatud kommentaarid

--- proceduur.sql
+++ proceduur.sql
@@ -10,7 +10,7 @@ spGetEmployees
 exec spGetEmployees
 execute spGetEmployees
 
---- 
+--- valib töötajaid nende soo ja osakonna alusel
 create proc spGetEmployeesByGenderAndDepartment
 @Gender nvarchar(20),
 @DepartmentId int
@@ -26,7 +26,7 @@ spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
---?
+-- otsib töötajate arvu nende soo alusel
 create proc spGetEmployeeCountByGender
 @Gender nvarchar(20),
 @EmployeeCount int output
@@ -34,7 +34,7 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- ?
+-- kontrollib, kas andmebaasis on naistöötajaid
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
@@ -48,7 +48,7 @@ declare @TotalCount int
 exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
 print @TotalCount
 
----?
+---tagastada töötajate koguarv
 create proc spTotalCount2
 @TotalCount int output
 as begin
@@ -59,7 +59,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
---- ?
+--- tagastada töötaja vastav eesnimi
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
@@ -67,19 +67,19 @@ as begin
 	select @FirstName = FirstName from employees where Id = @Id
 end
 
---?
+--töötaja ID 6 andmebaasis, prindib tema eesnime
 declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+--hankige töötaja eesnimi, edastades argumendina tema isikutunnistuse
 create proc spGetNameById2
 @Id int
 as begin
 	return (select FirstName from Employees where Id = @Id)
 end
 
--- ?
+-- hankige tabelimuutujast eesnimi ja printige see
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
 print 'Name of the employee = ' + @EmployeeName
