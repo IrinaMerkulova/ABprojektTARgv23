31c633ba0fe9232cebe69c27a5e1ba780df52b41
Erwin <ervin.sults@gmail.com>
2024-04-15 15:17:38 +0300

    9 komentaar tehtud

--- proceduur.sql
+++ proceduur.sql
@@ -59,7 +59,7 @@ declare @TotalEmployees int
 execute spTotalCount2 @TotalEmployees output
 select @TotalEmployees
 
----loome protseduur mis tagastab nimi from id 
+---loome protseduur mis tagastab nimi from id (9)
 create proc spGetNameById1
 @Id int,
 @FirstName nvarchar(50) output
