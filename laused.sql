365cae42e13cb71ee9c095dcce4ed94393ee3ceb
Erwin <ervin.sults@gmail.com>
2024-04-15 15:00:23 +0300

    20 komentaar tehtud

--- laused.sql
+++ laused.sql
@@ -119,7 +119,7 @@ select * from Person where Email like '%@%'
 select * from Person where Email not like '%@%'
 
 --- näitab, kelle on emailis ees ja peale @-märki
--- ainult üks täht
+-- ainult üks täht (20)
 select * from Person where Email like '_@_.com'
 
 -- naita inimene kellel esimesed tahed on was?
