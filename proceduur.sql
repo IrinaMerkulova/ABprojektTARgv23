3bc158c46e82a9b62c2a06efa3b29d61f0fe5e93
Erwin <ervin.sults@gmail.com>
2024-04-15 15:18:18 +0300

    12 komentaar tehtud

--- proceduur.sql
+++ proceduur.sql
@@ -79,7 +79,7 @@ as begin
 	return (select FirstName from Employees where Id = @Id)
 end
 
--- käivitamine protseduuri
+-- käivitamine protseduuri(12)
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
 print 'Name of the employee = ' + @EmployeeName
