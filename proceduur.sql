9e3f8d4ef840e3da8f926b76e7447b3261f7fd20
Erwin <ervin.sults@gmail.com>
2024-04-15 15:18:05 +0300

    11 komentaar tehtud

--- proceduur.sql
+++ proceduur.sql
@@ -72,7 +72,7 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
--- loome protseduur mis tagastab nimi from id 
+-- loome protseduur mis tagastab nimi from id (11)
 create proc spGetNameById2
 @Id int
 as begin
