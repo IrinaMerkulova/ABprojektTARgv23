1b486f4eda3073cf4b24b7d4cc3e5af08f4c0b7b
Erwin <ervin.sults@gmail.com>
2024-04-15 15:15:52 +0300

    5 komentaar tehtud

--- proceduur.sql
+++ proceduur.sql
@@ -34,7 +34,7 @@ as begin
 	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
 end
 
--- teeb protseduur, mis tagasatb female ?
+-- teeb protseduur, mis tagasatb female (5)
 declare @TotalCount int
 exec spGetEmployeeCountByGender 'Female', @TotalCount out
 if(@TotalCount = 0)
